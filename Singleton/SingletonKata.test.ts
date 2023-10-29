8bd28bee13de8b3bc680d147107360d92a329073
Hugo Flotts <hugoflotts@outlook.com>
2023-10-29 06:45:28 -0300

    feat(Singleton): add interface Configuration, singleton class Config, and App class. Add green test

--- Singleton/SingletonKata.test.ts
+++ Singleton/SingletonKata.test.ts
@@ -1,10 +1,22 @@
-import SingletonKata from './SingletonKata';
+import { Config, App } from './SingletonKata';
+
 
 describe('Singleton Pattern', () => {
   it('should create only one instance of Singleton', () => {
-    const instance1 = SingletonKata.getInstance();
-    const instance2 = SingletonKata.getInstance();
+    const instance1 = Config.getInstance();
+    const instance2 = Config.getInstance();
 
     expect(instance1).toBe(instance2);
   });
+
+  it('should change the config from the App', () => {
+    const config = Config.getInstance();
+    const app = new App(config);
+    app.setConfig('lang', 'ES');
+    expect(app.getConfig('lang')).toBe('ES');
+    app.setConfig('backgroundColor', '#fafafa');
+    expect(app.getConfig('backgroundColor')).toBe('#fafafa');
+    app.setConfig('lang', 'EN');
+    expect(app.getConfig('lang')).toBe('EN');
+  })
 });
--- Singleton/SingletonKata.ts
+++ Singleton/SingletonKata.ts
@@ -1,6 +1,50 @@
-
-class SingletonKata {
-  public static getInstance() {}
+interface Configuration {
+  set(key: string, value: string): void;
+  get(key: string): string;
 }
 
-export default SingletonKata;
+
+export class Config implements Configuration {
+  private static instance: Config | null = null;
+  private config: Record<string, string> = {};
+
+  private constructor () {}
+
+  public static getInstance() {
+    if (!Config.instance) {
+      Config.instance = new Config()
+    }
+    return Config.instance;
+  }
+
+  public set(key: string, value: string): void {
+    this.config[key] = value;
+  }
+
+  public get(key: string): string {
+    return this.config[key];
+  }
+}
+
+export class App {
+  private config: Configuration;
+
+  constructor (config: Configuration) {
+    this.config = config;
+  }
+
+  setConfig(key: string, value: string): void {
+    this.config.set(key, value);
+  }
+
+  getConfig(key: string): string {
+    return this.config.get(key);
+  }
+}
+
+const appConfig = Config.getInstance();
+const app = new App(appConfig);
+app.setConfig('language', 'ES');
+app.getConfig('language'); // 'ES'
+app.setConfig('language', 'EN');
+app.getConfig('language'); // 'EN'
